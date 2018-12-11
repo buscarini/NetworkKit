37e61f57c9fe78b008ddf653a64f183223e72f82
José Manuel Sánchez <buscarini@gmail.com>
2018-12-11 11:19:03 +0100

    Added test file, no tests yet!

---
+++ Tests/NetworkKitTests/NetworkKitTests.swift
@@ -0,0 +1,23 @@
+//
+//  NetworkKitTests.swift
+//  NetworkKit
+//
+//  Created by José Manuel Sánchez on 11/12/2018.
+//  Copyright © 2018 NetworkKit. All rights reserved.
+//
+
+import Foundation
+import XCTest
+import NetworkKit
+
+class NetworkKitTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        //// XCTAssertEqual(NetworkKitAlamofire().text, "Hello, World!")
+    }
+    
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
