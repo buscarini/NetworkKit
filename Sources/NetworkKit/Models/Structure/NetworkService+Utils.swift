b33568bc6c434ae74d05f28434eede80e272793d
José Manuel Sánchez <buscarini@gmail.com>
2018-12-11 12:01:16 +0100

    Fixed escaping parameters for networkservice request

--- Sources/NetworkKit/Models/Structure/NetworkService+Utils.swift
+++ Sources/NetworkKit/Models/Structure/NetworkService+Utils.swift
@@ -25,4 +25,37 @@ public extension NetworkService {
 		) -> CancelRequest {
 		return self.request(request, log: log, progress: { _, _ in }, completion: completion)
 	}
+	
+	public func request<T: Decodable>(
+		_ request: Request<T>,
+		log: @escaping Log,
+		progress: @escaping Progress,
+		completion: @escaping CompletionHandler<T>
+		) -> CancelRequest {
+		
+		return self.request(baseUrl, request.coerce(Data.self), log, progress, { networkResponse in
+			
+			switch networkResponse {
+				
+			case .success(let data, let response):
+				do {
+					let dataParsed = try T.parse(from: data)
+					let finalResponse = NetworkResponse.success(dataParsed, response)
+					completion(finalResponse)
+				} catch let error {
+					let finalResponse: NetworkResponse<T?> = NetworkResponse.encodingError(error)
+					completion(finalResponse)
+				}
+				
+			case .encodingError(let errror):
+				let finalResponse: NetworkResponse<T?> = NetworkResponse.encodingError(errror)
+				completion(finalResponse)
+				
+			case .networkError(let responseError, let response):
+				let finalResponse: NetworkResponse<T?> = NetworkResponse.networkError(responseError, response)
+				completion(finalResponse)
+			}
+			
+		})
+	}
 }
--- Sources/NetworkKit/Models/Structure/NetworkService.swift
+++ Sources/NetworkKit/Models/Structure/NetworkService.swift
@@ -6,45 +6,14 @@ public struct NetworkService {
 	public typealias CancelRequest = () -> Void
 	public typealias Progress = (UInt64, UInt64) -> Void
 
+	public typealias RequestFunction = (URL, Request<Data>, @escaping Log, @escaping Progress, @escaping CompletionHandler<Data>) -> CancelRequest
+	
 	public var baseUrl: URL
 	
-	public var request: (URL, Request<Data>, Log, Progress, CompletionHandler<Data>) -> CancelRequest
+	public var request: RequestFunction
 	
-	public init(baseUrl: URL, request: @escaping (URL, Request<Data>, Log, Progress, CompletionHandler<Data>) -> CancelRequest) {
+	public init(baseUrl: URL, request: @escaping RequestFunction) {
 		self.baseUrl = baseUrl
 		self.request = request
-	}
-	
-	public func request<T: Decodable>(
-		_ request: Request<T>,
-		log: @escaping Log,
-		progress: @escaping Progress,
-		completion: @escaping CompletionHandler<T>
-	) -> CancelRequest {
-		
-		return self.request(baseUrl, request.coerce(Data.self), log, progress, { networkResponse in
-			
-			switch networkResponse {
-				
-			case .success(let data, let response):
-				do {
-					let dataParsed = try T.parse(from: data)
-					let finalResponse = NetworkResponse.success(dataParsed, response)
-					completion(finalResponse)
-				} catch let error {
-					let finalResponse: NetworkResponse<T?> = NetworkResponse.encodingError(error)
-					completion(finalResponse)
-				}
-				
-			case .encodingError(let errror):
-				let finalResponse: NetworkResponse<T?> = NetworkResponse.encodingError(errror)
-				completion(finalResponse)
-				
-			case .networkError(let responseError, let response):
-				let finalResponse: NetworkResponse<T?> = NetworkResponse.networkError(responseError, response)
-				completion(finalResponse)
-			}
-			
-		})
 	}
 }
