46e1086ef7d4216c13b5926a31c570d75ec2e28b
José Manuel Sánchez <buscarini@gmail.com>
2018-12-17 10:44:09 +0100

    Changed progress from uint to int

--- Sources/NetworkKit/Models/Structure/NetworkService.swift
+++ Sources/NetworkKit/Models/Structure/NetworkService.swift
@@ -4,7 +4,7 @@ public struct NetworkService {
 	public typealias Log = (String) -> ()
 	public typealias CompletionHandler<T> = (_ result: NetworkResponse<T?>) -> Void
 	public typealias CancelRequest = () -> Void
-	public typealias Progress = (UInt64, UInt64) -> Void
+	public typealias Progress = (Int64, Int64) -> Void
 
 	public typealias RequestFunction = (URL, Request<Data>, @escaping Log, @escaping Progress, @escaping CompletionHandler<Data>) -> CancelRequest
 	
