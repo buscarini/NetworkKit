b2352a52ad11b4ff35652fe59adf94cbd196a2dd
José Manuel Sánchez <buscarini@gmail.com>
2019-01-10 13:16:17 +0100

    Added other parameters encoding method to allow customization

--- Sources/NetworkKit/Models/Structure/ParametersEncoding.swift
+++ Sources/NetworkKit/Models/Structure/ParametersEncoding.swift
@@ -1,6 +1,36 @@
 import Foundation
 
-public enum ParametersEncoding: String, Equatable, Hashable, CaseIterable {
+public enum ParametersEncoding {
     case url
     case json
+	case other((URLRequest, [String: Any]) -> URLRequest)
 }
+
+extension ParametersEncoding: Equatable {
+	public static func == (left: ParametersEncoding, right: ParametersEncoding) -> Bool {
+		switch (left, right) {
+		case (.url, .url):
+			return true
+		case (.json, .json):
+			return true
+		case (.other, .other):
+			return true
+		default:
+			return false
+		}
+	}
+}
+
+extension ParametersEncoding: Hashable {
+	public var hashValue: Int {
+		switch self {
+		case .url:
+			return "url".hashValue
+		case .json:
+			return "json".hashValue
+		case .other:
+			return "other".hashValue
+		}
+	}
+}
+
