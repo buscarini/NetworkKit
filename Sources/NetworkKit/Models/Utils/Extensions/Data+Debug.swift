5b8b5bf05f710003df6881ddc53b6645b6abdef4
José Manuel Sánchez <buscarini@gmail.com>
2019-01-11 10:08:37 +0100

    Fix crash

--- Sources/NetworkKit/Models/Utils/Extensions/Data+Debug.swift
+++ Sources/NetworkKit/Models/Utils/Extensions/Data+Debug.swift
@@ -4,7 +4,7 @@ extension Data {
     
     var debugString: String {
         // Try to parse to json to debug
-        if let jsonDict = try? JSONSerialization.jsonObject(with: self, options: .allowFragments) {
+        if JSONSerialization.isValidJSONObject(self), let jsonDict = try? JSONSerialization.jsonObject(with: self, options: .allowFragments) {
             if let jsonData = try? JSONSerialization.data(withJSONObject: jsonDict, options: .prettyPrinted) {
                 if let jsonString = String(data: jsonData, encoding: .utf8) {
                     return jsonString
