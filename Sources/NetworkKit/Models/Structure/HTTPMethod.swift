99002ce1b6640ba3d5a4292cdf38792558cf466f
José Manuel Sánchez <buscarini@gmail.com>
2020-08-18 14:07:12 +0200

    Added missing http methods

--- Sources/NetworkKit/Models/Structure/HTTPMethod.swift
+++ Sources/NetworkKit/Models/Structure/HTTPMethod.swift
@@ -1,9 +1,13 @@
 import Foundation
 
 public enum HTTPMethod: String, Equatable, Hashable, CaseIterable {
-    case get
-    case post
-    case put
-    case delete
+	case get
+	case head
+	case post
+	case put
+	case delete
+	case connect
+	case options
+	case trace
 	case patch
 }
